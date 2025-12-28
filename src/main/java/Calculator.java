22ed42ca5984e45f14ddaa4e83042d0c02e3b9a2
Ram-789357 <apparao455@gmail.com>
2025-12-28 23:10:20 +0530

    change test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
     public int add(int a, int b) {
-        return 8;
+        return a+b;
     }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,6 +7,6 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals(8, calc.add(2, 3));
+        assertEquals(5, calc.add(2, 3));
     }
 }
